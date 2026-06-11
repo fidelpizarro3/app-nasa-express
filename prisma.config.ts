6f7032b1f53441c839b9a89c14c99f9faddec87e
Fidel Pizarro <fidelpizarro11@gmail.com>
2026-06-11 00:40:30 -0300

    ejecute npx prisma db seed y se insertaron correctamente los datos en la tabla de la bd

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "npx tsx prisma/seed.js",
+    seed: "node prisma/seed.js",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
